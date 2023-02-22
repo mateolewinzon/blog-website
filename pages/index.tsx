dba5e6c9dcb5fd1740202ad441f42125516249b5
unknown <mateolewinzon@gmail.com>
2023-02-22 19:07:55 -0300

    add slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Container from "@/components/Container";
-import { twMerge } from "tailwind-merge";
 import styles from "styles/Home.module.css";
 import { GetStaticProps } from "next";
 import { client } from "@/lib/sanity";
 import { GET_POSTS } from "@/lib/queries";
 import { Post } from "@/lib/types";
 import PostCard from "@/components/PostCard";
+import PostsCarousel from "@/components/PostsSlider";
 
 type Props = {
   posts: Post[];
@@ -15,11 +15,7 @@ export default function Home({ posts }: Props) {
   return (
     <Container>
       <div className={styles["bg-radial-red-cyan"]}>
-        <section
-          className={twMerge(
-            "bg-radial-red-cyan flex justify-center py-4 mt-20 w-full items-center"
-          )}
-        >
+        <section className="flex justify-center py-4 mt-20 w-full items-center">
           <div className="flex flex-col gap-1 w-full px-4 py-6 sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-5xl">
             <h1 className="font-bold text-4xl">Our blog</h1>
             <h2 className="text-grey">
@@ -29,8 +25,11 @@ export default function Home({ posts }: Props) {
             </h2>
           </div>
         </section>
-        <section>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-5xl">
+        <section className="mx-auto p-4 sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-5xl">
+          <PostsCarousel posts={posts} />
+        </section>
+        <section className="mx-auto p-4 my-5 sm:max-w-2xl md:max-w-3xl lg:max-w-4xl xl:max-w-5xl">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {posts.map((post) => (
               <PostCard post={post} />
             ))}
